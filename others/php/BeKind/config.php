337ed48aef18f225663d43fcc90f24c0df7e13c7
Jesse <45318633+janim2@users.noreply.github.com>
2020-06-05 23:51:10 +0000

    Update config.php

--- others/php/BeKind/config.php
+++ others/php/BeKind/config.php
@@ -6,7 +6,7 @@
 */
 
 try {
-	$con /*just a variable to hold the connection and use in all pages*/ = new PDO('mysql:host=localhost; dbname=id2958862_bekind1', 'id2958862_projectbekind'/*username*/, 'b3K!nD2o2o20'/*password*/);
+	$con /*just a variable to hold the connection and use in all pages*/ = new PDO('mysql:host=localhost; dbname=namehere', 'usernamehere'/*username*/, 'passwordhere'/*password*/);
 	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //This prevents PDO from displaying the default error and the error's behaviour
 }catch (PDOException $e/*This catches an exception or error when trying to connect to database and casts the error to $e*/){
 	die("Sorry... Encountered a database problem."); //die kills the page. ie. the rest of the page won't work when you require or include in php.
